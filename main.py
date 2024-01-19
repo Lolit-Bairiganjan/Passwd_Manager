0c21cddb5333d23d5024e7e23413631b3175792a
Lolit Bairiganjan <wolfrainegaming.you@gmail.com>
2024-01-19 19:44:14 +0530

    Removed bug where password input was going to different database

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import mariadb
-mydb = mariadb.connect(host = 'localhost', user = 'root', passwd = 'lolit007', database = 'test_prep')
+mydb = mariadb.connect(host = 'localhost', user = 'root', passwd = 'lolit007', database = 'passwd_manager')
 mycr = mydb.cursor()
+
+
+##Constants
+global value
 
 
 ##Taking input for NEW/EXISTING USER
 begin = input(">New User?(1) or Existing User?(2):\n-----------------------------------\n>")
+
 
 ##Function Definitions
 
@@ -22,6 +27,7 @@ def new_user():
         else:
             print("Wrong input. Try Again!!")
 
+
 #function takes the input of password in right way
 def passwd_input():
     loop = 't'
@@ -35,6 +41,7 @@ def passwd_input():
         else:
             print("Password Accepted")
             loop = 'f'
+
 
 #function to create a username/password
 def create_user_passwd():
@@ -59,14 +66,42 @@ def profile_created():
     print("\n~Profile succesfully created~")
 
 
-#Function Calls
-if begin == '1':
-    new_user()
+#function for accessing the various roots associated with existing user
+def existing_user():
+    existing_user_input = input("\nAccess all username/passwords?(1), Add a website username/pasword?(2), Access particular website password?(3):\n--------------------------------------------------------------------------------------------------------------\n>")
+    if existing_user_input == '1':
+        access_all()
+
+#function to Add New Website Passwords
+def add_new():
+    value = mycr.execute("INSERT INTO [TABLE] VALUES()")
+
+
+#function to access a particular password
+def access_particular():
+    value = mycr.execute(f"SELECT * FROM [TABLE] WHERE [CONDITION]")
+
+
+#function to access all username/password of websites
+def access_all():  
+    value = mycr.execute(f'SELECT * FROM PROFILE')
+    for i in value:
+        for j in i:
+            print(j,end='')
+
 
 
 
 try:
     mycr.execute("SELECT * FROM LIBRARY")
+    #Function Calls
+    if begin == '1':
+        new_user()
+    elif begin == '2':
+        existing_user()
+    else:
+        pass
+
     
 except mariadb.Error as e:
     print(f"Error: {e}")
