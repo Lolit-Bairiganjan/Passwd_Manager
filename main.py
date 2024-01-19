aab3bdf2637e670221103ad02762042309f47a29
Lolit Bairiganjan <wolfrainegaming.you@gmail.com>
2024-01-19 18:09:09 +0530

    New/Existing User Function Created

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import mariadb
 mydb = mariadb.connect(host = 'localhost', user = 'root', passwd = 'lolit007', database = 'test_prep')
 mycr = mydb.cursor()
+#Taking input for NEW/EXISTING USER
+begin = input("New User?(1) or Existing User?(2):\n----------------------------------\n>")
+
+#Function Definitions
+def new_user():
+    print("~Welcome New User~")
+
+
+#Function Calls
+new_user()
+
+
+
 try:
     mycr.execute("SELECT * FROM LIBRARY")
-    x = mycr.fetchall()
-    print(x, 'Mariadb prints values in the form of list of tuples!')
+    
 except mariadb.Error as e:
     print(f"Error: {e}")
